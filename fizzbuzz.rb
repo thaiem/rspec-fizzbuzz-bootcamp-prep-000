9599f655ca9dcd3c9487a5c98df765934c13dde1
Elaine Thai <ethai1827@gmail.com>
2019-07-27 20:03:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,8 +4,8 @@ def fizzbuzz(int)
   if int % 15 == 0
     "FizzBuzz"
   elsif int % 3 == 0
-    "Buzz"
-  elsif int % 15 == 0
-      "FizzBuzz"
+    "Fizz"
+  elsif int % 5 == 0
+      "Buzz"
   end
 end
